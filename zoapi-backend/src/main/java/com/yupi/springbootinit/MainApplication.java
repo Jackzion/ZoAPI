7e6a3f99ac32dcbe4d2d5e137c7dd4ff84635348
ziio. <1934255852@qq.com>
2024-06-10 01:17:47 +0800

    对注释，类信息统一优化

--- zoapi-backend/src/main/java/com/yupi/springbootinit/MainApplication.java
+++ zoapi-backend/src/main/java/com/yupi/springbootinit/MainApplication.java
@@ -12,8 +12,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 /**
  * 主类（项目启动入口）
  *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ * @author <a href="https://github.com/Jackzion">ziio.</a>
+ * 
  */
 // todo 如需开启 Redis，须移除 exclude 中的内容
 @SpringBootApplication()
